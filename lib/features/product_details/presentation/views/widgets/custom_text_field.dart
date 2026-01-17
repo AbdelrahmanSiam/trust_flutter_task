fdd650925299a485355d9ed1e5e4be790c2e0984
Abdulrahman <syamabdo382@gmail.com>
2026-01-17 18:54:17 +0200

    add prefix icon to text field

--- lib/features/product_details/presentation/views/widgets/custom_text_field.dart
+++ lib/features/product_details/presentation/views/widgets/custom_text_field.dart
@@ -4,21 +4,21 @@ import 'package:trust_flutter_task/core/utils/app_styles.dart';
 
 class CustomTextField extends StatelessWidget {
   const CustomTextField({
-    super.key, required this.hintText,
+    super.key, required this.hintText,this.isPressed= false
   });
 final String hintText;
+final bool isPressed;
   @override
   Widget build(BuildContext context) {
     return TextField(
       textAlign: TextAlign.right,
       decoration: InputDecoration(
+        prefixIcon: Icon(Icons.delete_outline_outlined,color:isPressed? Colors.red : Colors.red.shade200,size: 30,),
         hintStyle: AppStyles.textStyles16,
         hintText: hintText,
-        filled: true,
-        fillColor: Colors.white,
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10),
-          borderSide: BorderSide.none,
+          borderSide: BorderSide(width: 5),
         ),
       ),
     );
