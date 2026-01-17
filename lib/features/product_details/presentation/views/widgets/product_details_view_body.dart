440e7d686bb5431ac2e08d2f45195daa4bd74874
Abdulrahman <syamabdo382@gmail.com>
2026-01-17 16:51:12 +0200

    create product view

---
+++ lib/features/product_details/presentation/views/widgets/product_details_view_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailsViewBody extends StatelessWidget {
+  const ProductDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        
+      ],
+    );
+  }
+}
--- lib/features/products/presentation/views/products_view.dart
+++ lib/features/products/presentation/views/products_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trust_flutter_task/features/product_details/presentation/views/widgets/product_details_view_body.dart';
 import 'package:trust_flutter_task/features/products/presentation/views/widgets/custom_bottom_nav_bar.dart';
 import 'package:trust_flutter_task/features/products/presentation/views/widgets/product_view_body.dart';
 
@@ -10,12 +11,12 @@ class ProductsView extends StatefulWidget {
 }
 
 class _ProductsViewState extends State<ProductsView> {
-    int selectedIndex = 1;
+    int selectedIndex = 0;
 
     final List<Widget> pages = const [
-    Center(child: Text("الرئيسية")),        
-    Center(child: Text("القائمة")),
-    ProductViewBody(),  
+    Center(child: Text("الرئيسية")),  
+    ProductViewBody(),      
+    ProductDetailsViewBody(),
     Center(child: Text("العروض")),
     Center(child: Text("الحساب")),
   ];
